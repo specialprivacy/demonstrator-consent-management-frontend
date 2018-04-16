9850eefa853a36f061a441f7cca81aa1ad1800fd
Arnaud SJongers <asjongers@gmail.com>
2018-04-16 10:22:46 +0200

    renamed policy fields

--- app/models/policy.js
+++ app/models/policy.js
@@ -1,9 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  dataAtom: DS.attr('string'),
-  locationAtom: DS.attr('string'),
-  processAtom: DS.attr('string'),
-  purposeAtom: DS.attr('string'),
-  recipientAtom: DS.attr('string')
+  dataCollection: DS.attr('string'),
+  locationCollection: DS.attr('string'),
+  processCollection: DS.attr('string'),
+  purposeCollection: DS.attr('string'),
+  recipientCollection: DS.attr('string')
 });
