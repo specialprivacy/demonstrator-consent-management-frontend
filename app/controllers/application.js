4b96c04eab35b9c405e569d9dba0e168d730f045
Nóra Szepes <nora.szepes@tenforce.com>
2018-04-24 15:54:23 +0200

    added login on application page

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,20 @@
 import Controller from '@ember/controller';
+import config from '../config/environment';
 
 export default Controller.extend({
+  actions: {
+    login() {
+      window.location.replace(`${config.auth.loginUrl}` +
+          `client_id=${config.auth.clientId}` +
+          `&response_type=${config.auth.responseType}`+
+          `&grant_type=${config.auth.grantType}` +
+          `&redirect_uri=${config.auth.loginRedirectUri}`
+        );
+    },
+    logout(){
+      window.location.replace(`${config.auth.logoutUrl}` +
+        `&redirect_uri=${config.auth.logoutRedirectUri}`
+      );
+    }
+  }
 });
