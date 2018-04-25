83beace1c5e24ff77e3c4d78497c8139c718d859
Arnaud SJongers <asjongers@gmail.com>
2018-04-25 12:00:32 +0200

    changed name of header

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   headers: {
-    'APP_KEY': "d5aca7a6-ed5f-411c-b927-6f19c36b93c3"
+    "Application-Id": "d5aca7a6-ed5f-411c-b927-6f19c36b93c3"
   }
 });
