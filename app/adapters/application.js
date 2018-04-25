d8b27971fd006dbee1b6c02ebca59b87b285815d
Nóra Szepes <nora.szepes@tenforce.com>
2018-04-25 15:46:29 +0200

    aded auth code to header

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,20 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  headers: {
-    "Application-Id": "d5aca7a6-ed5f-411c-b927-6f19c36b93c3"
-  }
+  session: Ember.inject.service('session'),
+  authCode: Ember.computed('session', 'session.isAuthenticated', function() {
+    if (this.get('session.isAuthenticated')) {
+      var authorizationCode = this.get('session.data.authenticated.authorizationCode');
+      if (!Ember.isEmpty(authorizationCode)) {
+        return authorizationCode;
+      }
+    }
+    return "";
+  }),
+  headers: Ember.computed('authCode', function(){
+    return {
+      "Application-Id": "d5aca7a6-ed5f-411c-b927-6f19c36b93c3",
+      "Authorization-Code": this.get('authCode')
+    };
+  })
 });
