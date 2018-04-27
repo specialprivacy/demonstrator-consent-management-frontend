a747eb2a4a53cf93c67f557d88af37afd977eaeb
Arnaud S'Jongers <arnaud.sjongers@tenforce.com>
2018-04-27 13:35:15 +0000

    Changed rootURL

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'consent-management-frontend',
     environment,
-    rootURL: '/',
+    rootURL: 'consent/',
     locationType: 'auto',
     backendUrl: 'http://localhost',
     frontendUrl: 'http://localhost',
